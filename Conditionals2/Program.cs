3a7374070e8ef9ef9ab454f1ab53c4ddcb61819a
Emmanuelle-Kirk <wolfeetried@gmail.com>
2021-10-04 12:31:27 -0300

    Initial commit

---
+++ Conditionals2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditionals2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Conditionals 2");
+            Console.ReadKey(true);
+        }
+    }
+}
