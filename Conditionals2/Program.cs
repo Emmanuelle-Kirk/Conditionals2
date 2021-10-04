666b59f1d1ef3f56abf0fd712425b64b440e8650
Emmanuelle-Kirk <wolfeetried@gmail.com>
2021-10-04 12:39:25 -0300

    Update Program.cs

--- Conditionals2/Program.cs
+++ Conditionals2/Program.cs
@@ -9,11 +9,17 @@ namespace Conditionals2
     class Program
     {
         static int score = 0;
-        static int lives = 3;
+        static int lives = 3; // Lives LEFT
         static bool alive = true; // true if alive, false if dead
         static void Main(string[] args)
         {
             Console.WriteLine("Conditionals 2");
+            lives = lives - 1;
+            if (lives == 0)
+           {
+                lives = 0;
+                alive = false;
+           }
             Console.ReadKey(true);
         }
     }
